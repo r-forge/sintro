cb3ce270abf1d676b92dcee1a03a095b2c3577e4
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2008-08-16 08:07:39 +0000

    home page updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,66 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   "><head><!-- This is the project specific website template -->
 
+<!-- This is the project specific website template -->
+<!-- It can be changed as liked or replaced by other content -->
 
-  
-  
-  
-  
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>&lt;?php echo $group_name; ?&gt;</title>
+<?php
 
-  
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
 
-  
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
 
-  
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
 
-  
-  
-  
-  
-  
-  <link href="%3C?php%20echo%20$themeroot;%20?%3Estyles/estilo1.css" rel="stylesheet" type="text/css">
+<body>
 
-
-
-</head><body>';
-?&gt;
-
-
-
-  
-	
-	
-	
-  
-
-<!--- R-Forge Logo -->
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
-
-
-
-
-
-<tbody><tr><td>
-<a href="/"><img src="%3C?php%20echo%20$themeroot;%20?%3E/images/logo.png" alt="R-Forge Logo" border="0"> </a> </td> </tr>
-</tbody>
+<!--- R-Forge Logo --->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
-
-
-
-
-
 
 
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
-
 <h2>Statistical Computing: An Introduction to R</h2>
-
-
-
-
 
 <p>This is the web page accompanying the book</p>
 <p>
@@ -71,76 +43,30 @@
   <br>
 </p>
 
-
-
-
-
-
 <p>Course material and supplements for a compact course on statistical computing, including an introduction to <span style="font-weight: bold;">R</span>.<br>
 </p>
 
-
-
-
-
 <p style="font-style: italic; font-weight: bold; color: rgb(255, 0, 0);">This page is under construction.</p>
-
-
-
-
-
 
 <hr style="width: 100%; height: 2px;">
 <p>Time line: <br>
 &nbsp;&nbsp;&nbsp; Version 2.7.2 of <span style="font-weight: bold;">R</span> is scheduled for end of August 2008.<br>
 &nbsp;&nbsp;&nbsp; The draft for the book will be fixed some days later.</p>
 
-
-
-
-
 <p>&nbsp;&nbsp;&nbsp; Pre-release infomation for the book and course material is available upon request.<br>
 </p>
-
-
-
-
 
 <p>&nbsp;&nbsp;&nbsp; Publication is scheduled for Jan. 2009. &nbsp;  <br>
 </p>
 
-
-
-
-
 A previous German version of the course material is 
   <a href="http://www.cran.r-project.org/doc/contrib/Sawitzki-Einfuehrung.pdf">Einführung in R</a>. 
 This version will stay available in the <a href="http://www.cran.r-project.org/">CRAN</a> archive.
-  
-
-<p>
-</p>
-
-
-
-
-
-
 <br>
 <hr style="width: 100%; height: 2px;">
-
-
-
-
-<p>For the <strong>project summary page</strong> and contac information, see <a href="http://%3C?php%20echo%20$domain;%20?%3E/projects/%3C?php%20echo%20$group_name;%20?%3E/"><strong>here</strong></a>.<br>
+<p>For the <strong>project summary page</strong> and contact information, see <a href="http://%3C?php%20echo%20$domain;%20?%3E/projects/%3C?php%20echo%20$group_name;%20?%3E/"><strong>here</strong></a>.<br>
 </p>
-<p><br>
- </p>
 
 
-
-
-
-
-
-</body></html>
+</body>
+</html>
