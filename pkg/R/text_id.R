d86377af801b768c6f47bc68c2531f05a56d02bc
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-16 17:03:40 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@124 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/text_id.R
+++
@@ -1,8 +0,0 @@
-	text_id <- function(x, y, ind, labels.id = rownames(x), adj.x = TRUE, 
-          cex.id = 0.75, label.pos = c(4, 2),...) {
-          x <- x[ind];y <- y[ind];labels.id <- labels.id[ind]
-	    labpos <- 
-                if(adj.x) label.pos[1 + as.numeric(x > mean(range(x)))] else 3
-	    text(x, y, labels.id, cex = cex.id, xpd = TRUE, 
-		 pos = labpos, offset = 0.25,...)
-	}
