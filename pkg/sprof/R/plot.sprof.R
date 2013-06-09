2d23dcd8e122027f5ce34b373f235d63d821843c
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-06-09 13:35:27 +0000

    plot, print added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/sprof/R/plot.sprof.R
@@ -0,0 +1,45 @@
+# $HeadURLs:$
+# setwd("")
+# source('~/Documents/lectures/src/insider/profile/sprof/pkg/R/plot_prof.R', chdir = TRUE)
+#! To Do
+#!
+
+
+
+plot_nodes <- function(x){
+	sn <- summary_nodes(x)
+	plot(sn)
+	invisible(sn)
+}
+
+plot_stacks <- function(x){
+	ss <- summary_stacks(x)
+	plot(ss)
+	invisible(ss)
+	}
+
+#plot_terminals<- function(x){
+#	table(x$stacks$stackleafnodes)
+#	x$stacks$refcount
+#}
+	
+
+plot_profiles <- function(x){
+	 sp <- summary_profiles(x)
+	 nrprof <- length(x$data)
+	z <- x$data; dim(z)=c(nrprof,1)
+	image(x=1:nrprof,y=1,z, 
+	xlab="event", 
+	ylab="", yaxt="n" , ylim=c(1,nrprof), 
+	col=rainbow(length(x$stacks$stacks)), 
+	main=sp$id)
+	#image(z, xlab="event", ylab="" , xlim=c(1,length(x$data)), yaxs=NULL)
+	invisible(x)
+}
+
+plot.sprof <- function(x){
+	plot_nodes(x)
+	plot_stacks(x)
+	plot_profiles(x)
+	invisible(x)
+}
---
+++ pkg/sprof/R/print.sprof.R
@@ -0,0 +1,38 @@
+# $Head:$
+# setwd("")
+# source('~/Documents/lectures/src/insider/profile/sprof/pkg/R/print_prof.R', chdir = TRUE)
+#! To Do
+#!
+
+
+
+print_nodes <- function(x){
+	sn <- summary_nodes(x)
+	print(sn)
+	invisible(sn)
+}
+
+print_stacks <- function(x){
+	ss <- summary_stacks(x)
+	print(ss)
+	invisible(ss)
+	}
+
+#print_terminals<- function(x){
+#	table(x$stacks$stackleafnodes)
+#	x$stacks$refcount
+#}
+	
+
+print_profiles <- function(x){
+	sp <- summary_profiles(x)
+	print(sp)
+	invisible(sp)
+}
+
+print.sprof <- function(x){
+	print_nodes(x)
+	print_stacks(x)
+	print_profiles(x)
+	invisible(x)
+}
