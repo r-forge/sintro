b392036c14f7332c06519cde280c44e73847b357
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-06-22 15:50:17 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@141 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/sprof/R/list.as.matrix.R
@@ -0,0 +1,16 @@
+# $HeadURL:$
+# convert list to matrix
+# list entries go to matrix columns, filled for equal length 
+
+list.as.matrix <- function(x, filler=NA){
+	maxlen <- max(sapply(x,length))
+    sapply(x, 
+	function(xs){fillen <- maxlen-length(xs)
+		if (fillen>0) {c(xs,rep(filler,fillen))} else xs
+	})
+}
+
+
+# x <- list(x1=c(1,2,3),x2=3, x3=4:8)
+# list.as.matrix(x)
+# list.as.matrix(x,filler=0)
