23fe0bab1c93a329d09d40ebbec7493efad7563e
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-07-14 20:45:53 +0000

    working on factor handling
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@171 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/sprof/tests/list.as.matrix.R
@@ -0,0 +1,33 @@
+# $HeadURL: svn+ssh://gsawitzki@svn.r-forge.r-project.org/svnroot/sintro/pkg/sprof/R/list.as.matrix.R $
+# convert list to matrix
+# list entries go to matrix columns, filled for equal length 
+
+x <- list(x1=c(1,2,3),
+	x2=3, 
+	x3=4:8)
+
+list.as.matrix(x)
+list.as.matrix(x,filler=0)
+
+xf <- list(x1=factor(c(1,2,3)),
+	x2=factor(3, levels =c(1,2,3,4), labels=c("L1","L2", "L3", "L4")),
+	x3=4:8)
+	
+list.as.matrix(xf)
+list.as.matrix(xf,filler=0)
+
+# c( factor(c(1,2,3)), factor(3, levels =c(1,2,3,4), labels=c("L1","L2", "L3", "L4")))
+#[1] 1 2 3 3
+# str(c( factor(c(1,2,3)), factor(3, levels =c(1,2,3,4), labels=c("L1","L2", "L3", "L4"))))
+# int [1:4] 1 2 3 3
+
+list.as.matrixt <- function(x, filler=NA){
+	maxlen <- max(sapply(x,length))
+    lapply(x, 
+	function(xs){fillen <- maxlen-length(xs)
+		if (fillen>0) {c(xs,rep(filler,fillen))} else xs
+	})
+}
+
+list.as.matrixt(xf)
+list.as.matrixt(xf,filler=0)
---
+++ pkg/sprof/tests/rrle.R
@@ -0,0 +1,27 @@
+#
+# setwd("")
+#! To Do
+#!
+
+
+
+x <- matrix(c(
+	1,1,1,2,2,
+	3,3,4,4,4,
+	5,5,6,6,7,
+	8,9,9,0,0
+),nrow=4, ncol =5, byrow=TRUE)
+xrrle <- rrle(x)
+xrrle
+
+t(sapply(xrrle, inverse.rle))
+
+
+
+xf <- as.factor(x)
+xfi <- levels(xf)
+xrrlef <- rrle(x)
+xrrlef1 <- lapply(xrrlef, function(xl) {xl$values <- factor(xl$values, levels=xfi); xl})
+xrrlef1
+
+t(sapply(xrrlef, inverse.rle))
