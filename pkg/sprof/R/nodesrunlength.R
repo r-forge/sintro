acaa7fccb72805a53b2020bfd58800dc986513cf
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-08-10 16:42:39 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@202 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sprof/R/nodesrunlength.R
+++ pkg/sprof/R/nodesrunlength.R
@@ -8,15 +8,20 @@
 # source('~/Documents/lectures/src/insider/profile/sprof/pkg/R/nodesrunlength.R', chdir = TRUE)
 #! To Do
 #!
-nodesrunlength <- function(sprof){
-profile_nodes_rlearray <- nodesprofile(sprof)
-mt <- margin.table(profile_nodes_rlearray, margin = c(1,3))
-#amt <- addmargins(mt)
-mts <- apply(mt,1,sum)
-mtt <- apply(mt,1,function (x) {sum(x*seq(along.with=x))})
-mtav <- mtt/mts
-amt=cbind(mt,count=mts, total_time=mtt, avg=mtav)
-amt <- amt[amt[,"count"]>0,]
-amt <- amt[order(amt[,"total_time"], decreasing=TRUE),]
-return(amt)
+nodesrunlength <- function(sprof, clean=TRUE){
+	profile_nodes_rlearray <- nodesprofile(sprof)
+	mt <- margin.table(profile_nodes_rlearray, margin = c(1,3))
+
+	mts <- apply(mt,1,sum)
+	mtt <- apply(mt,1,function (x) {sum(x*seq(along.with=x))})
+	mtav <- mtt/mts
+	amt=cbind(mt,nr_runs=mts, total_time=mtt, avg_time=mtav)
+	if (clean) {
+		amt <- amt[amt[,"nr_runs"]>0,]
+		amt <- amt[order(amt[,"total_time"], decreasing=TRUE),]
+	} else
+	{
+		while (dim(amt)[1]< sprof$info$nrnodes ) amt <- rbind(amt,0)
+	}
+	return(amt)
 }#nodesrunlength
