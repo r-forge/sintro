e653fff25c9e4e1caf36cef31fe07b11bbf15095
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-08-26 19:55:39 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@224 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sprof/R/asfactormodel.R
+++ pkg/sprof/R/asfactormodel.R
@@ -12,16 +12,18 @@
 #! clean up
 
 asfactormodel <- function(x, factormodel) {
-	
+
+# for a factor, the labels of the levels are called levels, 
+#	and the level indicators are called labels.	
 	if (is.factor(factormodel)) {
-		levels <- levels(factormodel)
-		labels <- labels(factormodel)
+		labels <- levels(factormodel)
+		levels <- labels(factormodel)
 		ordered <- is.ordered(factormodel)
 	} else if  (is.character(factormodel)) {
 		levels <- 1: length(factormodel)
 		labels <- factormodel
 		ordered = is.ordered(factormodel)
-	} else stop("xfactor: factormodel is of unknown type")
+	} else stop("xfactor: factormodel is of unsupported type")
 	
 	if (is.list(x)) {
 		sapply(x, 
