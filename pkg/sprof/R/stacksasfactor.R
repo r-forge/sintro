e7c9239eee788c8bc6a7689225fb648e5a7d86d0
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-08-26 20:09:31 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sprof/R/stacksasfactor.R
+++ pkg/sprof/R/stacksasfactor.R
@@ -1,8 +1,8 @@
-# $HeadURL: svn+ssh://gsawitzki@svn.r-forge.r-project.org/svnroot/sintro/pkg/sprof/R/stacksasfactor.R $
-# %$Id: barplot_s.R 213 2013-08-19 19:48:17Z gsawitzki $
-# %$Revision: 213 $
-# %$Date: 2013-08-19 21:48:17 +0200 (Mon, 19 Aug 2013) $
-# %$Author: gsawitzki $
+# $HeadURL$
+# %$Id$
+# %$Revision$
+# %$Date$
+# %$Author$
 # %$Name:$
 #
 # source('~/projects/rforge/sintro/pkg/sprof/R/stacksasfactor.R', chdir = TRUE)
