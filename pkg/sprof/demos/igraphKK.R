cea33960e0378f673b0a5ce37e9f5d227479b4dc
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-08-30 17:02:37 +0000

    test for isolating grphic experiments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/sprof/demos/igraphKK.R
@@ -0,0 +1,47 @@
+#$HeadURL: svn+ssh://gsawitzki@svn.r-forge.r-project.org/svnroot/sintro/pkg/sprof/tests/rrle.R $
+#$Id: rrle.R 178 2013-07-23 19:13:13Z gsawitzki $
+#$Revision: 178 $
+#$Date: 2013-07-23 21:13:13 +0200 (Tue, 23 Jul 2013) $
+#$Author: gsawitzki $
+
+# source('~/projects/rforge/sintro/pkg/sprof/tests/igraphKK.R', chdir = TRUE)
+
+# setwd("")
+#! To Do
+#!
+library(sprof)
+if (require("igraph")) {
+	
+data(sprof01lm)
+sprof <- sprof01lm
+
+as_igraph_sprof <- function(sprof, layoutfun, params=NULL,...){
+	adj <- adjacency(sprof)
+	adj[adj!=0] <-1
+	sprof_igraph <- graph.adjacency(adj)
+	sprof_igraph <- set.graph.attribute(sprof_igraph, "layout", 
+		layoutfun(sprof_igraph,params=params,...),...)
+	V(sprof_igraph)$color <- "yellow"
+	E(sprof_igraph)$color <- "#0000FF20"
+	E(sprof_igraph)$width <- c(1,2)
+	return(sprof_igraph)
+}
+
+#<<>>=
+sprof_igraph <- graph.adjacency(adjacency(sprof))
+sprof_igraph <- set.graph.attribute(sprof_igraph, 
+	"layout", layout.kamada.kawai(sprof_igraph))
+V(sprof_igraph)$color <- "yellow"
+#E(sprof_igraph)$width <- c(1,2)
+E(sprof_igraph)$color <- "#0000FF40"
+E(sprof_igraph)$width <- c(1,2)
+# see 
+#<<fig=TRUE, label=sprof_igraphkamada,width=8, height=8>>=
+plot(sprof_igraph, 
+	main=paste0("igraph kamada.kawai layout\n", sprof$info$id))
+	legend("topleft", 
+		legend= paste0("class: ",class(sprof_igraph)),
+		bg="#FFFFE040",
+		seg.len=0
+		)
+} else warning("could not load igraph", immediate=TRUE)
