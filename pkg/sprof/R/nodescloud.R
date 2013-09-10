a3c9bb454349d7fd27bb6e9fdc613562509a0382
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2013-09-10 16:12:33 +0000

    corrected col table in nodescloud stabilized nodescloud scaled images for html help files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sintro@239 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sprof/R/nodescloud.R
+++ pkg/sprof/R/nodescloud.R
@@ -8,10 +8,24 @@
 # source('~/Documents/lectures/src/insider/profile/sprof/pkg/R/nodescloud.R', chdir = TRUE)
 #! To Do
 #!
-nodescloud <- function(sprof,min.freq=3,icol, col){
+nodescloud <- function(sprof, src, min.freq=3, 
+	icol, col, ...)
+{
 if (require(wordcloud)){
-	if (inherits(sprof,"sprof")) nodes <- sprof$nodes else nodes <- sprof
-	if (missing(icol)) icol <- rkindex(nodes$self.time, maxindex=12)
+	if (inherits(sprof,"sprof")) 
+		{	if (missing(src)) src <- sprof$info$id
+			nodes <- sprof$nodes 
+		} else {
+			if (missing(src)) src <- deparse(substitute(sprof))
+			nodes <- sprof
+		}
+	if (missing(icol)) {
+		if (is.null(nodes$icol))icol <- rkindex(nodes$self.time, maxindex=16) else
+			icol <- nodes$icol
+		}
+	
+	icol <- unclass(icol) # just to make sure...
+	
 	if (missing(col)) col <- terrain.colors(max(icol))
 	
 	wordcloud(nodes$name, 
@@ -20,7 +34,8 @@ if (require(wordcloud)){
 		random.order=FALSE,
 		rot.per=0.3,
 		colors = col[icol],
-		ordered.colors=TRUE)
+		ordered.colors=TRUE, ...)
+	title(sub=src, col.sub=grey(0.5),font=3)
 	} else {
 			frame()
 			legend("center","package wordcloud \n is required for this plot")
